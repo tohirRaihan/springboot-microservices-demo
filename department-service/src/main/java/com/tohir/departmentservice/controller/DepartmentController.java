1b050c7418a1c93b611ddcd11767f2b1dc4d6439
Tohir Raihan <tohir.raihan@gmail.com>
2024-03-25 12:26:16 +0600

    :pencil2: Add: DepartmentController

---
+++ department-service/src/main/java/com/tohir/departmentservice/controller/DepartmentController.java
@@ -0,0 +1,38 @@
+package com.tohir.departmentservice.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tohir.departmentservice.dto.DepartmentDto;
+import com.tohir.departmentservice.service.DepartmentService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("api/departments")
+@RequiredArgsConstructor
+public class DepartmentController {
+
+    private final DepartmentService departmentService;
+
+    // Build save department REST API
+    @PostMapping
+    public ResponseEntity<DepartmentDto> saveDepartment(@RequestBody DepartmentDto departmentDto) {
+        DepartmentDto savedDepartment = departmentService.saveDepartment(departmentDto);
+        return new ResponseEntity<>(savedDepartment, HttpStatus.CREATED);
+    }
+
+    // Build get department rest api
+    @GetMapping("{department-code}")
+    public ResponseEntity<DepartmentDto> getDepartment(@PathVariable("department-code") String departmentCode) {
+        DepartmentDto departmentDto = departmentService.getDepartmentByCode(departmentCode);
+        return new ResponseEntity<>(departmentDto, HttpStatus.OK);
+    }
+
+}
